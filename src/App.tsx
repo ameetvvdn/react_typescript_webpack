7eae2e0e7cdc96de854dd1422a9b0beff5bfb341
ameetvvdn <ameet.kumar@vvdntech.in>
2018-06-21 17:12:04 +0530

    lanswepper image added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './App.scss';
 
-import logo from './logo.svg';
+import logo from './logo.png';
 
 class App extends React.Component {
   public render() {
@@ -9,10 +9,9 @@ class App extends React.Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React </h1>
         </header>
         <p className='App-intro'>
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          Under Construction
         </p>
       </div>
     );
