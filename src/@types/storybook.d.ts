1d8633352c95bc18966d98334e170a9f1678fcb9
ameetvvdn <ameet.kumar@vvdntech.in>
2018-07-02 10:39:49 +0530

    structure changed

--- src/@types/storybook.d.ts
+++ src/@types/storybook.d.ts
@@ -3,4 +3,5 @@ declare module "@storybook/react/demo";
 declare module "@types/storybook__react";
 declare module "@storybook/react";
 declare module "react-relay";
+declare module "relay-runtime";
 
